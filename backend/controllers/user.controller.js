a25e7b2d76811073771d499dd5f39530b1689d56
devwithjustmaley <ahmadsouleymane1302@gmail.com>
2026-03-22 23:15:28 +0000

    Updates

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -12,8 +12,8 @@ const signAndSendToken = (res, user) => {
   const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "Lax",
+    secure: true,
+    sameSite: "None",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 };
